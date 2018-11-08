cd24e5c384fbaaf43c39dc06b8823dd85d29994b
Ashish Rao <ashishrao2598@gmail.com>
2018-11-08 21:52:36 +0530

    add props

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import AddFishForm  from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
-    return <div className="inventory"></div>
+    return <div className="inventory">
+      <AddFishForm addFish={this.props.addFish}/>
+    </div>
   }
 }
 

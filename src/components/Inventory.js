766723c19eed08b0883d878076f04f36d4e21fcb
Ashish Rao <ashishrao2598@gmail.com>
2018-11-09 11:50:13 +0530

    add props

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -5,6 +5,7 @@ class Inventory extends React.Component {
   render() {
     return <div className="inventory">
       <AddFishForm addFish={this.props.addFish}/>
+      <button onClick={this.props.loadSampleFishes}>Load Sample</button>
     </div>
   }
 }

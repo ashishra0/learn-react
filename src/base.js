eee65837e8bda1ea3240bb891f7b36353283c339
Ashish Rao <ashishrao2598@gmail.com>
2018-11-17 16:56:01 +0530

    remove api-key

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDscYcbok18ER3liuY_dZoq9z87_ekg8j4",
+  apiKey: "",
   authDomain: "cotd-react-ashishrao.firebaseapp.com",
   databaseURL: "https://cotd-react-ashishrao.firebaseio.com"
 });
